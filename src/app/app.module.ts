da876ca1ad762ec86c5c8c767f0f90cdb7a1834a
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-02-02 01:00:00 +0200

    feature/ add global styles and expand app.module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/appComponent/app.component';
+import { InputComponent } from './components/inputComponent/input.component';
+import { EyeSvg } from './components/eyeSvgComponent/eyeSvg.component';
+import { IndicatorWrapper } from './components/indicatorWrapperComponent/indicatorWrapper.component';
+import { CrossedOutEyeSvg } from './components/crossedOutEyeComponent/crossedOutEye.component';
+import { FormsModule } from '@angular/forms';
+import { PasswordReliabilityPipe } from './pipes/password-status.pipe';
+import { Indicator } from './components/indicatorComponent/indicator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    EyeSvg,
+    IndicatorWrapper,
+    Indicator,
+    CrossedOutEyeSvg,
+    PasswordReliabilityPipe,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
