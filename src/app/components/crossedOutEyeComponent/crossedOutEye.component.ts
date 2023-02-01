94587e9dcc4ced1036ba6c61cf8d349eead2acbb
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-02-01 23:38:37 +0200

    feature/ create components with svg

---
+++ src/app/components/crossedOutEyeComponent/crossedOutEye.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crossed-out-eye-svg',
+  templateUrl: './crossedOutEye.component.svg',
+})
+export class CrossedOutEyeSvg {}
---
+++ src/app/components/eyeSvgComponent/eyeSvg.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eye-svg',
+  templateUrl: './eyeSvg.component.svg',
+})
+export class EyeSvg {}
